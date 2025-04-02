ca0e35e23c422cc2d88679788712fb1a40330f40
GregorioCavulla::ghigo <101806375+GregorioCavulla@users.noreply.github.com>
2025-04-02 10:02:29 +0200

    Update FondAI

---
+++ FondamentiDiAI/Esercitazioni/ConigliettoDiCioccolato/main.java
@@ -0,0 +1,29 @@
+/*
+ * Created on Apr 2, 2025
+ *
+ */
+package ai.search.mc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import aima.search.framework.*;
+import aima.search.uninformed.*;
+import aima.search.informed.*;
+
+/**
+ *  @author Gregorio Cavulla
+ * 
+ *  problema missionari e cannibali
+ */
+
+ public class main {
+    public static void main(String[] args) {
+        
+
+    } // main
+ }
+
